264e642fe505137687ab0478fb69468b834dfa7b
Brando <brandoflores16@gmail.com>
2020-12-14 01:01:25 -0700

    Initial Commit

--- SeeFood/ViewController.swift
+++ SeeFood/ViewController.swift
@@ -6,14 +6,74 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    @IBOutlet weak var imageView: UIImageView!
+    let imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        imagePicker.delegate = self
+        
+        // Choose how to use the camera button
+        //imagePicker.sourceType = .camera
+        imagePicker.sourceType = .photoLibrary
+        
+        
+        imagePicker.allowsEditing = false
+    }
+    
+    // Tell view controller that user has picked an image
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let userPickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            imageView.image = userPickedImage
+            
+            // Convert to Core Image Image (CII)
+            // Allows us to user COREML to get an interpretation from it
+            guard let ciimage = CIImage(image: userPickedImage) else {
+                fatalError("Could not convert CIImage")
+            }
+            print("second")
+            detect(image: ciimage)
+        }
+
+        imagePicker.dismiss(animated: true, completion: nil)
+        
+        print("third")
+    }
+    
+    private func detect(image: CIImage) {
+        
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else {
+            fatalError("Loading COREML model failed")
+        }
+        
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            guard let results = request.results as? [VNClassificationObservation] else {
+                fatalError("Model failed to process image")
+            }
+            print(results)
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        
+        do {
+            try! handler.perform([request])
+        }
+        catch {
+            print(error.localizedDescription)
+        }
+        
     }
 
-
+    @IBAction func cameraButtonPressed(_ sender: UIBarButtonItem) {
+        print("first")
+        present(imagePicker, animated: true, completion: nil)
+        
+    }
+    
 }
 
